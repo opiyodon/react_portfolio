b1b630956ff10df5ea440f5cf555cd90f938c0e0
Don Artkins <opiyodon9@gmail.com>
2023-01-18 12:22:58 -0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,css,php,js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
